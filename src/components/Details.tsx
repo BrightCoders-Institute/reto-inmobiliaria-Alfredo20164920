4a9fb31b7877cd494e2105f8f1378091e5554218
Alfredo20164920 <apaz20164920@gmail.com>
2023-06-13 22:54:12 -0600

    Feat: card details

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,27 +1,27 @@
 import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { faLocationDot, faBed, faBath, faMaximize } from '@fortawesome/free-solid-svg-icons';
+import Service from './Service';
+import ButtonLike from './ButtonLike';
 
 export default function Details() {
   return (
     <View style={styles.container}>
-      {/* TITLE */}
       <Text style={styles.title}>The Willows</Text>
-      {/* SUBTITLE */}
       <View style={styles.containerLocation}>
         <FontAwesomeIcon icon={faLocationDot} />
         <Text style={styles.subTitle}>3517 W. Gray St. Utica</Text>
       </View>
-      {/* ICONS */}
-      <Text>Icon</Text>
-      {/* PRICE */}
-      <View style={styles.containerBottom}>
-        <Text>Price</Text>
-        <Text>Button like</Text>
+      <View style={styles.containerServices}>
+        <Service text={'3'} icon={faBed}/>
+        <Service text={'2'} icon={faBath}/>
+        <Service text={'230 ft'} icon={faMaximize}/>
       </View>
-      {/* BUTTON LIKE */}
-
+      <View style={styles.containerBottom}>
+        <Text style={styles.price}>$440/m</Text>
+        <ButtonLike />
+      </View>
     </View>
   );
 }
@@ -42,7 +42,6 @@ const styles = StyleSheet.create({
       fontWeight: '600',
       fontSize: 24,
       color: '#000',
-      marginBottom: 5,
     },
     subTitle: {
       color: '#737373'
@@ -51,5 +50,14 @@ const styles = StyleSheet.create({
       display: 'flex',
       flexDirection: 'column',
       justifyContent: 'space-between',
+    },
+    containerServices: {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    price: {
+      fontSize: 20,
+      color: '#000'
     }
   });
